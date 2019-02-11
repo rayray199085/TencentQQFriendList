3ac3ed144b67373245467a4cc6711608d9d3116a
Rui Cao <rayray199085@gmail.com>
2019-02-12 09:02:41 +1100

    Initial Commit

---
+++ TecentQQFriendList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TecentQQFriendList
+//
+//  Created by Stephen Cao on 12/2/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
